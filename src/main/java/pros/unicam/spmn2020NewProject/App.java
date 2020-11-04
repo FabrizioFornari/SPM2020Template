e0c8ab85d1cae03ac6db47b92b5a7840b20cd83f
FabrizioFornari89 <fabrizio.fornari@unicam.it>
2020-11-04 14:05:48 +0100

    Added Classes and Tests for practicing with JUnit

---
+++ src/main/java/pros/unicam/spmn2020NewProject/App.java
@@ -0,0 +1,13 @@
+package pros.unicam.spmn2020NewProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/pros/unicam/spmn2020NewProject/AppTest.java
@@ -0,0 +1,21 @@
+package pros.unicam.spmn2020NewProject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
