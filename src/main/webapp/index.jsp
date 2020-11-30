1f21e4088511026a279be130b8dcdfc5753945e2
FabrizioFornari89 <fabrizio.fornari@unicam.it>
2020-11-30 12:36:49 +0100

    added a test for mywebapp

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,4 +1,5 @@
 <html>
+<title>SPM 2020</title>
 <body>
 <h2>Hello World!</h2>
 Ciao a Tutti!
---
+++ src/test/java/pros/unicam/spm2020/practice/Selenium/MyWebappSeleniumTest.java
@@ -0,0 +1,77 @@
+package pros.unicam.spm2020.practice.Selenium;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+class MyWebappSeleniumTest {
+
+
+	static String browser;
+	static String projectPath;
+	static String pathToMacDrivers;
+	
+	static WebDriver driver;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		 projectPath = System.getProperty("user.dir");
+		 
+	}
+	
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {		
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+		
+		if(System.getProperty("os.name").equals("Mac OS X")) {
+		System.setProperty("webdriver.chrome.driver", projectPath+"/drivers/mac/chromedriver");
+		}
+		if(System.getProperty("os.name").contains("Windows")) {
+			System.setProperty("webdriver.chrome.driver", projectPath+"\\drivers\\windows\\chromedriver.exe");
+		}
+	    
+		ChromeOptions chromeOptions = new ChromeOptions();
+		driver = new ChromeDriver(chromeOptions);
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+		driver.close();
+		driver.quit();
+	}
+	
+	@Test
+	void test() {
+		  driver.navigate().to("http://apromore.unicam.it/spmn2020NewProject");
+	      System.out.println("Title is: "+driver.getTitle());
+	      assertTrue(driver.getTitle().contains("SPM 2020"));
+	}
+
+}
