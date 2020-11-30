d69af197a8bb58b8182414b02a5acbe53da4dcbe
FabrizioFornari89 <fabrizio.fornari@unicam.it>
2020-11-30 20:08:24 +0100

    added a failing test

--- src/test/java/pros/unicam/spm2020/practice/TestAfterDeploy/HeadlessChromeTestAcceptance.java
+++ src/test/java/pros/unicam/spm2020/practice/TestAfterDeploy/HeadlessChromeTestAcceptance.java
@@ -91,7 +91,7 @@ public class HeadlessChromeTestAcceptance
   void testMyAppTitle() {
 	  driver.navigate().to(address);
 	  System.out.println("Title is: "+driver.getTitle());
-	  assertTrue(driver.getTitle().contains("SPM 2020"));
+	  assertTrue(driver.getTitle().contains("SPM 20203"));
   }
 
   
